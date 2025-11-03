e9c57c1761ddd2b3115cb121fe3ea0eba90a6da6
dydghkehwkd159-eng <dydghkehwkd159@gmail.com>
2025-11-04 00:49:30 +0900

    Replace youtube_dl with yt_dlp

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import asyncio
-import youtube_dl
+import yt_dlp as youtube_dl
 import random
 
 intents = discord.Intents.default()
